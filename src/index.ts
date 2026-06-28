e19c1fc1e5e0ae6bf9c783c50d0fe20df766d2b2
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2026-06-28 13:49:10 +0530

    entry point created

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { default as AsyncAPI } from './containers/AsyncAPI';
+export type { IAsyncAPIProps } from './containers/AsyncAPI';
+
+export type { ConfigInterface } from './config';
+export { defaultConfig } from './config';
+
+export { useAsyncAPIDocument } from './contexts';
+
+export { SchemaTree, SchemaTab } from './components/schema';
+export type { SchemaTreeProps } from './components/schema';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ include: ['src'] }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'aui',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `aui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
