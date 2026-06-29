7ec379a25dbef7b5f5c8f154ea4d090eb988783d
acethecreator <devaze007@gmail.com>
2026-06-29 19:12:58 +0100

    fix: typo

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export type { IAsyncAPIProps } from "./containers/AsyncAPI/AsyncAPI";
 export type { ConfigInterface } from "./config/config";
 export type { AsyncAPIDocumentData } from "./types/schema";
 export { parseAndRender } from "./helpers/parser";
-export { AsyncApiViewer } from "./containers/AsyncAPI/AsyncAPIRenderer";
+export { AsyncAPIRenderer } from "./containers/AsyncAPI/AsyncAPIRenderer";
